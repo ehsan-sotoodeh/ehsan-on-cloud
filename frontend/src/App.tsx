506e324633fc68417579ff0d93814e2913c336f5
Ehsan Sotoodeh <ehsansotoodeh@Ehsans-MacBook-Pro.local>
2025-02-07 16:08:48 -0800

    Test husky setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-
-
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css'
+import "./App.css";
 
-import TODOList from './TodoList';
+import TODOList from "./TodoList";
 
 function App() {
-
   return (
     <>
       <TODOList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
